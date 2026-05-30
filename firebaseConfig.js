60afd9d9bd84422c678717e55e56c7db932816b9
HelioMattos <143033762+HelioMattos@users.noreply.github.com>
2026-05-30 12:28:07 -0300

    atualizacao 1.1

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,28 @@
+// firebaseConfig.js (EXCLUSIVO PARA O APP DE CELULAR)
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNzzytLbr6emlkNF9TdmoVc_9tVHxaYvw",
-  authDomain: "saquainfo-77137.firebaseapp.com",
-  projectId: "saquainfo-77137",
-  storageBucket: "saquainfo-77137.firebasestorage.app",
-  messagingSenderId: "468842142028",
-  appId: "1:468842142028:web:f24561e7b4da8f83c657e9"
+  apiKey: "AIzaSyCkwKFsktwQjWBlOISsmmXum3wJ-jT3JYo",
+  authDomain: "saquainfo-99128.firebaseapp.com",
+  projectId: "saquainfo-99128",
+  storageBucket: "saquainfo-99128.firebasestorage.app",
+  messagingSenderId: "50771678769",
+  appId: "1:50771678769:web:82ce83d38282c7cba06710"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-export const auth = initializeAuth(app, {
+// Proteção crucial para evitar tela vermelha (Blob Error) no Android/iOS
+global.XMLHttpRequest = global.originalXMLHttpRequest || global.XMLHttpRequest;
+
+const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage)
 });
 
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
+
